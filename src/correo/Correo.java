492446e4a07c3a7dd8db2b37b68b45d9659bc7ca
Fedehub-cpu <fedecuadrat@gmail.com>
2022-03-16 00:45:35 +0100

    Se agrega el proyecto correo

---
+++ src/correo/Correo.java
@@ -0,0 +1,16 @@
+package correo;
+
+/**
+ *
+ * @author Fede
+ */
+public class Correo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
